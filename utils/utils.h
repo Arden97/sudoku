92fa5baae0b0d2a1b97383df8a0e8941decf03c5
Artem Denisov <Artemden97@mail.ru>
2020-08-02 17:04:51 +0200

    Update utils.h

--- utils/utils.h
+++ utils/utils.h
@@ -18,21 +18,30 @@
 
 /// Counts rows of the loaded game file
 int count_rows(FILE *file);
+
 /// Counts columns of the loaded game file
 int count_columns(FILE *file);
+
 /// Prints the current game board
 void print_board(int **matrix, int rows, int columns);
+
 /// Loads a file with a prepared game board 
 int **load_game(FILE *file, int rows, int columns);
+
 /// Saves a game board to file
 void save_game(int **matrix, int rows, int columns);
+
 /// Creates a new game
 int **new_game(int rows, int columns);
+
 /// Prints an error message, if the board contains it.
 void error();
-/// Checks the game board for errors and empty fields 
+
+/// Checks the game board for errors and empty fields
 int check_board(int **matrix, int rows, int columns, int game_mode);
+
 /// Finishes the game
 void end_game(int **matrix, int rows, int columns, int size);
+
 /// Starts the game
 int **start_game(int **matrix, int rows, int columns, int size);
